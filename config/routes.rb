58b3dcb6b4962d4cfab728fcddeed65169acc9a3
Per Evensen <per.evensen@gmail.com>
2016-11-15 16:40:25 -0500

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_ember_app :frontend, to: "/"
   resources :movements
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
